ff67651893cf947b2c500d7fb71e407001f50f26
Ruben Rivas <rrivas@utec.edu.pe>
2019-12-01 20:54:23 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <string_view>
+#include "lodepng.h"
+using namespace std;
+
+std::vector<unsigned char> decode(string_view filename,
+    unsigned int& width, unsigned int& height) {
+
+    std::vector<unsigned char> result;
+    unsigned error = lodepng::decode(result, width, height, filename.data());
+    if (error) {
+        std::cerr << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    }
+    return result;
+}
+
+void encode(string_view filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
+    unsigned error = lodepng::encode(filename.data(), image, width, height);
+    if (error) {
+        std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    }
+}
+
+int main()
+{
+    unsigned int w;
+    unsigned int h;
+    auto image = decode("../in.png", w, h);
+    // Un comment if you want to check buffer content
+    for (size_t i = 0; i < h; i++) {
+        for (size_t j = 0; j < w * 4; j += 4) {
+            int r = image[i * w * 4 + j + 0]; // Red component
+            int g = image[i * w * 4 + j + 1]; // Green component
+            int b = image[i * w * 4 + j + 2]; // Blue component
+            int a = image[i * w * 4 + j + 3]; // Alpha component
+            std::cout << r << " ";
+            std::cout << g << " ";
+            std::cout << b << " ";
+            std::cout << a << "|";
+        }
+        std::cout << endl;
+    }
+    encode("../out.png", image, w, h);
+    return 0;
+}
